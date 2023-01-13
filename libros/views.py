4d5f31e2c8bbe4f732ffc1915c82aab5ba1160b5
joseusa192 <joseusa192@gmail.com>
2023-01-12 22:12:06 -0500

    Modeficacion de plantilla y muestreo de datos

--- libros/views.py
+++ libros/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Libro
 
 def book_list(request):
-    return render(request, './libros/book_list.html',{})
+    libro = Libro.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, './libros/book_list.html',{'libro':libro})
