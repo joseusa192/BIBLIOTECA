5c4810b76f36785c1ac524caf8a88b2baa0f78fa
joseusa192 <joseusa192@gmail.com>
2023-01-13 15:57:16 -0500

    Despliegue de plantillas

--- libros/urls.py
+++ libros/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_list, name='lista_libros')
+    path('', views.book_list, name='lista_libros'),
+    path('libro/<int:pk>/', views.libro_detail, name='libro_detail'),
 ]
--- libros/views.py
+++ libros/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Libro
 
 def book_list(request):
     libro = Libro.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, './libros/book_list.html',{'libro':libro})
+
+def libro_detail(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+    return render(request, 'libros/detalle_libro.html', {'libro': libro})
