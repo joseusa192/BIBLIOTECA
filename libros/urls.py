94b6a85c2bded4fffa71ab32f4ce3b31b400cdad
joseusa192 <joseusa192@gmail.com>
2023-01-15 22:15:46 -0500

    Despliegue

--- libros/urls.py
+++ libros/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.book_list, name='lista_libros'),
     path('libro/<int:pk>/', views.libro_detail, name='libro_detail'),
+    path('libro/new/', views.new_book, name='new_book'),
+    path('libro/<int:pk>/edit', views.book_edit, name='book_edit'),
 ]
--- libros/views.py
+++ libros/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Libro
+from .forms import LibroForm
 
 def book_list(request):
     libro = Libro.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +10,34 @@ def book_list(request):
 def libro_detail(request, pk):
     libro = get_object_or_404(Libro, pk=pk)
     return render(request, 'libros/detalle_libro.html', {'libro': libro})
+
+def form_book(request):
+    form = LibroForm()
+    return render(request, 'libros/book_edit.html',{'form':form})
+
+def new_book(request): 
+    if request.method=="POST":
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            libro = form.save(commit=False)
+            libro.autor=request.user
+            libro.published_date = timezone.now()
+            libro.save()
+            return redirect('libro_detail', pk=libro.pk)
+    else: 
+        form = LibroForm()
+    return render(request, 'libros/book_edit.html', {'form':form})
+
+def book_edit(request, pk): 
+    libro = get_object_or_404(Libro, pk=pk)
+    if request.method=="POST":
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            libro = form.save(commit=False)
+            libro.autor=request.user
+            libro.published_date = timezone.now()
+            libro.save()
+            return redirect('libro_detail', pk=libro.pk)
+    else: 
+        form = LibroForm(instance=libro)
+    return render(request, 'libros/book_edit.html', {'form':form})
